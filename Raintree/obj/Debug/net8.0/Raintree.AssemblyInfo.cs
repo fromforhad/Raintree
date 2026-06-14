54428969491fe919a71cdf362b347759ac61efd1
Forhad Rahman <forhadrahman100@gmail.com>
2026-06-14 20:58:32 +0600

    tweaked the fixed height for mobile

--- Raintree/obj/Debug/net8.0/Raintree.AssemblyInfo.cs
+++ Raintree/obj/Debug/net8.0/Raintree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Raintree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d76e3b21f8cab83efc6c097006a802dca4f6f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+311fa35ea740dd495dd781a4be8c6a154a8acf6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Raintree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Raintree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
