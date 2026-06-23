d1cb13baf30392d27721f82717e9172e61b89c77
Forhad Rahman <forhadrahman100@gmail.com>
2026-06-23 17:23:18 +0600

    Test cache busting for HTML page update

--- Raintree/obj/Debug/net8.0/Raintree.AssemblyInfo.cs
+++ Raintree/obj/Debug/net8.0/Raintree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Raintree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d275c8d791f1b1897bfa5a43c9fe524eaf27110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad93b67713db3e402dc218162c0db247b215dbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Raintree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Raintree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
