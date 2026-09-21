7cf0d0aa5f791ce3f0d73f1e7e5e507abc15d9e6
Forhad Rahman <forhadrahman100@gmail.com>
2026-09-21 19:34:28 +0600

    commit before backup

--- Raintree/obj/Debug/net8.0/Raintree.AssemblyInfo.cs
+++ Raintree/obj/Debug/net8.0/Raintree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Raintree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf134f7ea215fea4809881b7658988c2de996429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1dbb522f9edf23a1ed405bacb6da19676b4afeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Raintree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Raintree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
